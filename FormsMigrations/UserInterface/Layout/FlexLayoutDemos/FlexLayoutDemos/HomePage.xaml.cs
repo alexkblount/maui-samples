86975eb521ee9d81b17686f22f528ce1084912af
Alexey Strakh <alexey.strakh@microsoft.com>
2021-08-10 17:21:05 -0700

    simple flex layout sample added

---
+++ FormsMigrations/UserInterface/Layout/FlexLayoutDemos/FlexLayoutDemos/HomePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace FlexLayoutDemos
+{
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+        {
+            InitializeComponent ();
+
+            NavigateCommand = new Command<Type>(
+                async (Type pageType) =>
+                {
+                    Page page = (Page)Activator.CreateInstance(pageType);
+                    await Navigation.PushAsync(page);
+                });
+
+            BindingContext = this;
+        }
+
+        public ICommand NavigateCommand { private set; get; }
+	}
+}
