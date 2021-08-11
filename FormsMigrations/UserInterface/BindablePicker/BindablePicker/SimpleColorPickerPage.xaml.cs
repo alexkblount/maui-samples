dc1fb000aa138ad5b747999c63718fd76404600c
dean-msft <defaizal@microsoft.com>
2021-08-11 13:27:57 -0400

    Converted BindablePicker sample

---
+++ FormsMigrations/UserInterface/BindablePicker/BindablePicker/SimpleColorPickerPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace BindablePicker
+{
+	public partial class SimpleColorPickerPage : ContentPage
+	{
+		public SimpleColorPickerPage()
+		{
+			InitializeComponent();
+			BindingContext = new SimpleColorPickerPageViewModel();
+		}
+	}
+}
---
+++ FormsMigrations/UserInterface/BindablePicker/BindablePicker/SimpleColorPickerPageCS.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace BindablePicker
+{
+	public class SimpleColorPickerPageCS : ContentPage
+	{
+		public SimpleColorPickerPageCS()
+		{
+			var picker = new Picker { Title = "Select a color" };
+			picker.SetBinding(Picker.ItemsSourceProperty, "ColorNames");
+			picker.SetBinding(Picker.SelectedItemProperty, "SelectedColorName", mode: BindingMode.TwoWay);
+
+			var boxView = new BoxView { HeightRequest = 200 };
+			boxView.SetBinding(BoxView.ColorProperty, "SelectedColor");
+
+			Content = new StackLayout
+			{
+				Margin = new Thickness(20),
+				Children = {
+					new Label { Text = "Bindable Picker Demo", FontAttributes = FontAttributes.Bold, HorizontalOptions = LayoutOptions.Center },
+					picker,
+					boxView
+				}
+			};
+
+			BindingContext = new SimpleColorPickerPageViewModel();
+		}
+	}
+}
