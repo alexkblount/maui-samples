85d3dda67b937444bf2e573aab6c12a6d9fcc132
dean-msft <defaizal@microsoft.com>
2021-08-11 14:53:09 -0400

    Converted checkbox demos

---
+++ FormsMigrations/UserInterface/CheckBoxDemos/CheckBoxDemos/BasicCheckBoxBindingsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace CheckBoxDemos
+{
+    public partial class BasicCheckBoxBindingsPage : ContentPage
+    {
+        public BasicCheckBoxBindingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FormsMigrations/UserInterface/CheckBoxDemos/CheckBoxDemos/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace CheckBoxDemos
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
