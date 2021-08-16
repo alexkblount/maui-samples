7be4ae038a2aaa7c46c0652c061948afd419f22e
Alexey Strakh <alexey.strakh@microsoft.com>
2021-08-16 16:40:19 -0700

    maui bindings sample (android)

---
+++ FormsMigrations/Bindings/NativeBindings/NativeBindings/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Com.Xamarin.Textcounter;
+
+namespace NativeBindings
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			int count = TextCounter.NumConsonants("Some random text 123!!!");
+			CounterLabel.Text = $"Consonants count: {count}";
+		}
+	}
+}
---
+++ FormsMigrations/Bindings/NativeBindings/NativeBindings/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace NativeBindings
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
