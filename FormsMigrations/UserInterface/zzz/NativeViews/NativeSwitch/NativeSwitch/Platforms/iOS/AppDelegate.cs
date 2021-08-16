3311a86fe39755401bdb174d4e6c4b580be5cfec
dean-msft <defaizal@microsoft.com>
2021-08-16 10:12:14 -0400

    Initial migration

---
+++ FormsMigrations/UserInterface/zzz/NativeViews/NativeSwitch/NativeSwitch/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace NativeSwitch
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
---
+++ FormsMigrations/UserInterface/zzz/RadioButtonDemos/RadioButtonDemos/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace RadioButtonDemos
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
